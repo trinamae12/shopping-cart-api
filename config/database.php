a2ab3976b669dbd160481c4843da3f89594a4356
trinamae12 <maetrina12@gmail.com>
2021-04-16 20:31:36 +0800

    Remove comment

--- config/database.php
+++ config/database.php
@@ -81,7 +81,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        //Staging db
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' =>  $host,
