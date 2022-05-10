8e283baf7dcb40b0aeb92b7489963f725de4d644
Trina Mae P. Abarquez <abarquez.tmp@AD.SPROBE.PH>
2022-05-10 11:57:34 +0800

    Add login and logout functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
+use Validator;
 
 class UserController extends Controller
 {
+    public $successStatus = 200;
+    
     public function index() {
         return User::all();
     }
 
     public function store(Request $request) {
-        return User::create($request->all());
+        // return User::create($request->all());
+        // $validator = Validator::make($request->all(), [
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'password' => 'required',
+        //     'c_password' => 'required|same:password',
+        // ]);
+        // if ($validator->fails()) {
+        //     return response()->json(['error'=>$validator->errors()], 401);            
+        // }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        return response()->json(['success'=>$success], $this->successStatus);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('users', [UserController::class, 'index']);
+Route::post('login', [ApiController::class, 'login']);
+// Route::post('register','ApiController@register');
 Route::post('users', [UserController::class, 'store']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('get-details', [ApiController::class, 'getDetails']);
+    Route::get('logout', [ApiController::class, 'logout']);
+});
+
+Route::get('users', [UserController::class, 'index']);
+
